06ede371bd6b3e67cea1ed7d960e5f5170e342bd
Abbie Sithole <sithole@thedigitalacademy.co.za>
2021-09-08 22:18:19 +0200

    routing

--- shopping-frontEnd/src/app/app-routing.module.ts
+++ shopping-frontEnd/src/app/app-routing.module.ts
@@ -1,7 +1,11 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { CartComponent } from './components/cart/cart.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', redirectTo: 'home', pathMatch: 'full' },
+  { path: 'Cart', component:CartComponent},
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
