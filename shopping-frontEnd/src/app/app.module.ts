6364219a19a63a6220ce2114b9bb1aa14e70fcf5
Abbie Sithole <sithole@thedigitalacademy.co.za>
2021-09-08 21:42:04 +0200

    components created

--- shopping-frontEnd/src/app/app.module.ts
+++ shopping-frontEnd/src/app/app.module.ts
@@ -3,10 +3,14 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NavBarComponent } from './nav-bar/nav-bar.component';
+import { CartComponent } from './cart/cart.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavBarComponent,
+    CartComponent
   ],
   imports: [
     BrowserModule,
