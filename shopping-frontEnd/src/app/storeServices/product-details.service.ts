8a2c5f876c2a9f5e6b3e40cad5a0d74c53cfd610
Abbie <79112224+AbbieS2175@users.noreply.github.com>
2021-10-13 10:52:53 +0200

    product from backend can now display

--- shopping-frontEnd/src/app/storeServices/product-details.service.ts
+++ shopping-frontEnd/src/app/storeServices/product-details.service.ts
@@ -1,19 +1,40 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import {map} from 'rxjs/operators';
+import { Observable } from "rxjs";
+
 @Injectable({
   providedIn: 'root'
 })
 export class ApiService 
 {
 
+  url: string = "http://localhost:3000/Product";
+  urlcategory:string="http://localhost:3000/Product/?category=";
+
   constructor(private http : HttpClient) { }
 
+  // getProduct(): Observable<any> 
+  // {
+  //   return this.http.get<any>(this.url);
+  // }
+
+  // getProduct()
+  // {
+  //   return this.http.get<any>("http://localhost:3000/Product")
+  //   .pipe(map((res:any)=>{
+  //     return res;
+  //   }))
+  // }
   getProduct()
   {
-    return this.http.get<any>("https://fakestoreapi.com/products")
+    return this.http.get<any>(this.url)
     .pipe(map((res:any)=>{
       return res;
     }))
+  }
+  getProductByCategory(category:any):Observable<any>
+  {
+    return this.http.get<any>(this.urlcategory+{category})
   }
 }
