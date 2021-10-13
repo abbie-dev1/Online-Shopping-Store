0285f864652c828b1c050c9a78748dce7bd48b26
Abbie <79112224+AbbieS2175@users.noreply.github.com>
2021-10-13 09:32:17 +0200

    product service done

--- nest-back-end/src/products/product/product.service.ts
+++ nest-back-end/src/products/product/product.service.ts
@@ -1,4 +1,26 @@
 import { Injectable } from '@nestjs/common';
+import { Model } from 'mongoose';
+import { InjectModel } from '@nestjs/mongoose';
+import { Product } from '../interfaces/product.interface';
 
 @Injectable()
-export class ProductService {}
+export class ProductService {
+    constructor(@InjectModel('Product') private readonly ProductModel: Model<Product>){}
+    async findAll(): Promise<Product[]>{
+        return await this.ProductModel.find();
+    }
+    async find(id: string): Promise<Product>{
+        return await this.ProductModel.findOne({ _id: id });
+    }
+    async Create(product: Product): Promise<Product>{
+        const newProduct = new this.ProductModel(product);
+        return await newProduct.save();
+    }
+    async update(id: string, product: Product): Promise<Product>{
+        
+        return await this.ProductModel.findByIdAndUpdate(id, product, { new: true});
+    }
+    async delete(id: string): Promise<Product>{
+        return await this.ProductModel.findByIdAndRemove(id);
+    }
+}
